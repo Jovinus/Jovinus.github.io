054966118d35e76e345abc086427e73a1dfc633e
Jovinus <lkh256@gmail.com>
2026-04-19 06:50:57 +0000

    Deploying to gh-pages from @ Jovinus/Jovinus.github.io@90bafc938ced67a8a8ba0cf2a9d02af0ed63295e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -390,16 +390,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-multicenter-study-on-deep-learning-models-for-acute-kidney-injury-prediction-including-development-and-external-validation-has-been-published-in-npj-digital-medicine",
+          title: 'Our multicenter study on deep learning models for acute kidney injury prediction, including...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
