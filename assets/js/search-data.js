4753bd96ae013a55993e6ed54b8b5a34187938f5
Jovinus <lkh256@gmail.com>
2026-04-19 07:55:19 +0000

    Deploying to gh-pages from @ Jovinus/Jovinus.github.io@5e1c7d6a5c12c7ac0efce6f0844633d0a5475819 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "people I work with and have mentored",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
